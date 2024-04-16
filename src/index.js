31d3d6bc23d554c29a054f12d4d5e43a5a3db270
itzaaditya <agarwal.aaditya2765@gmail.com>
2024-04-16 20:29:19 +0530

    5th test done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-// src/index.js
+const parseQuery = require('../src/queryParser');
 
-const parseQuery = require('./queryParser');
-const readCSV = require('./csvReader');
-
-async function executeSELECTQuery(query) {
-    const { fields, table, whereClause } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
-    
-    // Filtering based on WHERE clause
-    const filteredData = whereClause
-        ? data.filter(row => {
-            const [field, value] = whereClause.split('=').map(s => s.trim());
-            return row[field] === value;
-        })
-        : data;
-
-    // Selecting the specified fields
-    return filteredData.map(row => {
-        const selectedRow = {};
-        fields.forEach(field => {
-            selectedRow[field] = row[field];
-        });
-        return selectedRow;
+test('Parse SQL Query', () => {
+    const query = 'SELECT id, name FROM sample';
+    const parsed = parseQuery(query);
+    expect(parsed).toEqual({
+        fields: ['id', 'name'],
+        table: 'sample'
     });
-}
-
-module.exports = executeSELECTQuery;
+});
